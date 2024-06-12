7b655b8df21c3d2960bfacda8c90706c75bb1abb
ariful171042 <arif171042@gmail.com>
2024-06-12 20:13:07 +0600

    update

--- app/(home)/_components/DealCartSection.tsx
+++ app/(home)/_components/DealCartSection.tsx
@@ -1,68 +1,7 @@
-import DealCart from "@/app/Components/ui/DealCart";
 import Image from "next/image";
-import { title } from "process";
-import React from "react";
 import ProductSlider from "./ProductSlider";
 
-const data = [
-  {
-    id: 1,
-    title: "Portable Mini Water Air Cooler Table Fan - 10W",
-    img: "https://media.e-valy.com/cms/products/images/03b04398-2830-44e7-b7d1-56be656bb9b0?h=250&w=250",
-    price: 443,
-    currentprice: 2233,
-  },
-  {
-    id: 2,
-    title: "Wireless Bluetooth Earbuds with Charging Case",
-    img: " https://media.e-valy.com/cms/products/images/03b04398-2830-44e7-b7d1-56be656bb9b0?h=250&w=250",
-    price: 1200,
-    currentprice: 999,
-  },
-  {
-    id: 3,
-    title: "Smart Fitness Band with Heart Rate Monitor",
-    img: "https://media.e-valy.com/cms/products/images/9bfbc392-b0a4-4ed5-8878-7c8383b0343e",
-    price: 1500,
-    currentprice: 1299,
-  },
-  {
-    id: 4,
-    title: "Portable USB Rechargeable LED Desk Lamp",
-    img: "https://media.e-valy.com/cms/products/images/6589cf1d-d074-4fd5-ac66-95f1ea7d1a27?h=250&w=250",
-    price: 700,
-    currentprice: 599,
-  },
-  {
-    id: 5,
-    title: "Mini Wireless Keyboard with Touchpad",
-    img: "https://media.e-valy.com/cms/products/images/9bfbc392-b0a4-4ed5-8878-7c8383b0343e",
-    price: 850,
-    currentprice: 799,
-  },
-  {
-    id: 6,
-    title: "Electric Handheld Milk Clock Digital Frother",
-    img: "https://media.e-valy.com/cms/products/images/85d02fe7-2a8a-4aec-b0c5-34618157fa7f?h=250&w=250",
-    price: 2000,
-    currentprice: 1799,
-  },
-  {
-    id: 7,
-    title: "Digital Alarm Clock with FM Radio",
-    img: "https://media.e-valy.com/cms/products/images/8ec3f1ba-320c-4fdc-a0e8-e11933b731cf?h=250&w=250",
-    price: 900,
-    currentprice: 850,
-  },
-  {
-    id: 8,
-    title: "Electric Handheld Milk Frother",
-    img: "https://media.e-valy.com/cms/products/images/7600f06e-d71f-4592-a812-54a015d15cb4?h=250&w=250",
-    price: 500,
-    currentprice: 450,
-  },
-];
-
+import { data } from "@/data";
 const DealCartSection = () => {
   return (
     <section className="mt-5 bg-gradient-to-r from-indigo-500 to-red-300 p-3 rounded-xl ">
@@ -88,7 +27,7 @@ const DealCartSection = () => {
         <DealCart />
       </div> */}
 
-      <ProductSlider products={data} cartStatus="DealCart"/>
+      <ProductSlider products={data} cartStatus="DealCart" />
     </section>
   );
 };
--- app/(home)/_components/ProductSection.tsx
+++ app/(home)/_components/ProductSection.tsx
@@ -3,79 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const data = [
-  {
-    id: 1,
-    title: "Portable Mini Water Air Cooler Table Fan - 10W",
-    img: "https://media.e-valy.com/cms/products/images/03b04398-2830-44e7-b7d1-56be656bb9b0?h=250&w=250",
-    price: 443,
-    currentprice: 2233,
-  },
-  {
-    id: 2,
-    title: "Wireless Bluetooth Earbuds with Charging Case",
-    img: " https://media.e-valy.com/cms/products/images/03b04398-2830-44e7-b7d1-56be656bb9b0?h=250&w=250",
-    price: 1200,
-    currentprice: 999,
-  },
-  {
-    id: 3,
-    title: "Smart Fitness Band with Heart Rate Monitor",
-    img: "https://media.e-valy.com/cms/products/images/9bfbc392-b0a4-4ed5-8878-7c8383b0343e",
-    price: 1500,
-    currentprice: 1299,
-  },
-  {
-    id: 4,
-    title: "Portable USB Rechargeable LED Desk Lamp",
-    img: "https://media.e-valy.com/cms/products/images/6589cf1d-d074-4fd5-ac66-95f1ea7d1a27?h=250&w=250",
-    price: 700,
-    currentprice: 599,
-  },
-  {
-    id: 5,
-    title: "Mini Wireless Keyboard with Touchpad",
-    img: "https://media.e-valy.com/cms/products/images/9bfbc392-b0a4-4ed5-8878-7c8383b0343e",
-    price: 850,
-    currentprice: 799,
-  },
-  {
-    id: 6,
-    title: "Electric Handheld Milk Clock Digital Frother",
-    img: "https://media.e-valy.com/cms/products/images/85d02fe7-2a8a-4aec-b0c5-34618157fa7f?h=250&w=250",
-    price: 2000,
-    currentprice: 1799,
-  },
-  {
-    id: 7,
-    title: "Digital Alarm Clock with FM Radio",
-    img: "https://media.e-valy.com/cms/products/images/8ec3f1ba-320c-4fdc-a0e8-e11933b731cf?h=250&w=250",
-    price: 900,
-    currentprice: 850,
-  },
-  {
-    id: 8,
-    title: "Electric Handheld Milk Frother",
-    img: "https://media.e-valy.com/cms/products/images/7600f06e-d71f-4592-a812-54a015d15cb4?h=250&w=250",
-    price: 500,
-    currentprice: 450,
-  },
-  {
-    id: 9,
-    title: "Mini Wireless Keyboard with Touchpad",
-    img: "https://media.e-valy.com/cms/products/images/9bfbc392-b0a4-4ed5-8878-7c8383b0343e",
-    price: 850,
-    currentprice: 799,
-  },
-  {
-    id: 10,
-    title: "Electric Handheld Milk Clock Digital Frother",
-    img: "https://media.e-valy.com/cms/products/images/85d02fe7-2a8a-4aec-b0c5-34618157fa7f?h=250&w=250",
-    price: 2000,
-    currentprice: 1799,
-  },
-];
-
+import { data } from "@/data";
 const ProductSection = () => {
   return (
     <section className="mt-3 border border-gray/50 shadow-lg p-5  rounded-xl">
@@ -90,7 +18,8 @@ const ProductSection = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 xl:grid-cols-4 2xl: gird-cols-5 items-center lg:gap-2.5 ">
-          {data && data.map((product) => <Cart key={product.id} product={product} />)}
+          {data &&
+            data.map((product) => <Cart key={product.id} product={product} />)}
         </div>
       </div>
     </section>
